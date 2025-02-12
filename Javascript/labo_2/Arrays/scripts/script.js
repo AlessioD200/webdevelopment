0cbdf7d58af4252d143dc8decf029418a36f0e0b
AlessioD <106388894+AlessioD200@users.noreply.github.com>
2025-02-12 13:37:16 +0100

    Update design

---
+++ Javascript/labo_2/Arrays/scripts/script.js
@@ -0,0 +1,8 @@
+const setup = () => {
+    let namen = ['Alessio','Emma','Alyssa','Jonas','Thomas']
+    console.log(namen.length)
+    console.log(namen[0],namen[2],namen[4])
+    namen.push('Alessandro')
+    console.log(namen)
+}
+window.addEventListener("load", setup);
