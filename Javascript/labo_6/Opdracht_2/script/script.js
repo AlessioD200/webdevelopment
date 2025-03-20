bd808603b3e781570c2e4d8669c1b19514c0cfa5
AlessioD <106388894+AlessioD200@users.noreply.github.com>
2025-03-20 21:49:10 +0100

    Update script.js

--- Javascript/labo_6/Opdracht_2/script/script.js
+++ Javascript/labo_6/Opdracht_2/script/script.js
@@ -1,7 +1,6 @@
 const setup = () => {
     let colorChange = document.querySelector("ul");
     colorChange.style.color = "red";
-
     let nieuweAfbeelding = document.createElement("img");
     nieuweAfbeelding.src = "./img/test.png";
     document.body.appendChild(nieuweAfbeelding);
